93f943091cf8ff5bd03bcb9d776e5e4a7ed44fdb
mchlmrsm <73650446+mchlmrsm@users.noreply.github.com>
2023-11-17 05:48:12 +0300

    Added ovrelaping text to h/r - it's certainly more fun, but i do have to work more on it.

--- assets/js/hyperromanticism.js
+++ assets/js/hyperromanticism.js
@@ -1,18 +1,42 @@
-const visibleTextStyle = ''
-const invisibleTextStyle = 'text-indent: 100%;white-space: nowrap; overflow: hidden;'
 
-function displayFragment(event){
-    const oldFragmentDisplay = document.querySelector(".activeDisplay")
-    if(oldFragmentDisplay != null){
-        oldFragmentDisplay.classList.remove('activeDisplay')
-        oldFragmentDisplay.style = invisibleTextStyle
+
+function displayFragments(event){
+    
+    const checkboxes = document.getElementById("nameInput").getElementsByTagName('input')
+    for( const checkbox of checkboxes){
+        const fragmentDisplay = document.querySelector("."+checkbox.name)
+        if(checkbox.checked){
+            fragmentDisplay.classList.remove('hidden')
+        }
+        else{
+            fragmentDisplay.classList.add('hidden')
+        }
     }
-    const name = event.target.value;
-    const fragmentDisplay = document.querySelector("."+name)
-    fragmentDisplay.style = visibleTextStyle
-    fragmentDisplay.classList.add('activeDisplay')
 }
 
 
+// const charWidth = 5
+// const charHeight = 10
+
+// const background_set = '▓▒░▘▞▙▟▛▚▜▞▘▟▘▜▐▌█▊ '.split()
+// function moveBackground(){
+//     function generateStringFromSet(len, set){
+//         let str = ''
+//         for (let i = 0; i < len; i++) {
+//             str = str + set[~~(Math.random() * set.length)];;
+//         }
+//         return str
+//     }
+//     var height = document.body.clientHeight;
+//     var width = document.body.clientWidth;
+//     const background_len = height/charHeight * width/charWidth
+    
+//     document.querySelector('.background').innerHTML = generateStringFromSet(background_len, background_set)
+    
+// }
+
+
+// setInterval(moveBackground, 1000); // Move fish every 1 second
+
 const nameInput = document.getElementById("nameInput");
-nameInput.onchange = displayFragment
+nameInput.addEventListener("change", displayFragments);
